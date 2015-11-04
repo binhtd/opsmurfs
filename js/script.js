9fdbb034f3003cca6236eb3b2b5466ee541166db
trinh duc binh <trinhducbinh@gmail.com>
2015-11-04 12:08:48 +0700

    default show account for server EUW

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    $("#accounts .nav-pills li[data-filter='web'] a").trigger("click");
+
     $("div.thumbnail button.message-btn").click(function(){
         $("#form-content .contactbuyaccount #categoryid").val($(this).attr("data-id"))
         $("#form-content").modal();

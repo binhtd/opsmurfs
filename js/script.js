54becaf1ea2e4d828b197b4c73a781b61695d3ba
trinh duc binh <trinhducbinh@gmail.com>
2015-11-04 11:58:43 +0700

    rename popup class name

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("div.thumbnail button.message-btn").click(function(){
-        $("#form-content .contact #categoryid").val($(this).attr("data-id"))
+        $("#form-content .contactbuyaccount #categoryid").val($(this).attr("data-id"))
         $("#form-content").modal();
     });
 });
